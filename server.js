17144a073f3fb6a1d9ee7fe5a254ecb1a051e092
Cesar Gonzalez <gcesar13@yahoo.com>
2023-01-19 18:13:21 -0700

    adding db connection

--- server.js
+++ server.js
@@ -11,3 +11,14 @@ const server = new ApolloServer({
 const { url } = await startStandaloneServer(server);
 
 console.log(`🚀 Server ready at ${url}`);
+
+
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      // log where we can go to test our GQL API
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+};
