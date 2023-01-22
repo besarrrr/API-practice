92d0f9e1e29dea33daced9396dbc6b6f26310118
Cesar Gonzalez <gcesar13@yahoo.com>
2023-01-22 16:46:46 -0700

    adding user and thought typedefs and resolvers

--- Deep-Thoughts/server/schemas/resolvers.js
+++ Deep-Thoughts/server/schemas/resolvers.js
@@ -1,10 +1,36 @@
+const { User, Thought} = require('../models');
 
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'Hello World';
-        }
+        thoughts: async ( parent, { username}) => {
+            const params = username ? {username} : {};
+            return Thought.find(params).sort({ createdAt: -1});
+        },
+
+        thought: async (parent, { _id }) => {
+            return Thought.findOne({ _id });
+        },
+
+     // get all users
+        users: async () => {
+            return User.find()
+            .select('-__v -password')
+            .populate('friends')
+            .populate('thoughts');
+        },
+    // get a user by username
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+            .select('-__v -password')
+            .populate('friends')
+            .populate('thoughts');
+        },
     }
+
+    // Here, we pass in the parent as more of a placeholder parameter. It won't be used, but we need something in that first parameter's spot so we can access 
+    // the username argument from the second parameter. We use a ternary operator to check if username exists. If it does, we set params to an object with a 
+    // username key set to that value. If it doesn't, we simply return an empty object.
+
 };
 
 module.exports = resolvers;
