c6a7bd1a26ee77e29b3f844a7c4df683f1ac1407
Cesar Gonzalez <gcesar13@yahoo.com>
2023-01-23 07:46:05 -0700

    login auth added and jwt

--- Deep-Thoughts/server/schemas/resolvers.js
+++ Deep-Thoughts/server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { User, Thought} = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
@@ -35,8 +36,9 @@ const resolvers = {
     Mutation: {
         addUser: async (parent, args) => {
             const user = await User.create(args);
+            const token = signToken(user);
 
-            return user;
+            return { token, user };
 
         },
         login: async (parent, { email, password }) => {
@@ -52,7 +54,8 @@ const resolvers = {
               throw new AuthenticationError('Incorrect credentials');
             }
           
-            return user;
+            const token = signToken(user);
+            return { token, user };
           }
     }
 };
