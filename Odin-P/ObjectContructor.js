609ecf3dd461d8d9c0f43b245791b3428f170ce4
Cesar Gonzalez <gcesar13@yahoo.com>
2023-02-08 20:46:08 -0700

    secondary wat to write the function

--- Odin-P/ObjectContructor.js
+++ Odin-P/ObjectContructor.js
@@ -16,4 +16,26 @@ const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'yes')
 
 console.log(theHobbit.info());
 
- //  would put out   The Hobbit, J.R.R. Tolkien, 295 pages, Read: yes
+//  would put out   The Hobbit, J.R.R. Tolkien, 295 pages, Read: yes
+
+
+//Second way to write it: 
+
+//  function Book(title, author, pages, read) {
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+
+//     const bookTitle = this.title;
+//     const bookAuthor = this.author;
+//     const bookPages = this.pages;
+//     const bookRead = this.read;
+
+//     this.info = function() {
+//         return `${bookTitle}, ${bookAuthor}, ${bookPages} pages, Read: ${bookRead}`;
+//     };
+// }
+
+// const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'yes');
+// console.log(theHobbit.info());
