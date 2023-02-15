f952d198b856511d0bb73616f78a26ec0022e25d
Cesar Gonzalez <gcesar13@yahoo.com>
2023-02-14 21:19:17 -0700

    q6 string compression

--- Data Structures/examples.js
+++ Data Structures/examples.js
@@ -262,4 +262,34 @@ function oneAway(string1, string2) {
 }
 
 
-// queston 6 String compression: ex is taking string that is aabcccccaa into a2b1cfa3
+// question 6 String compression: ex is taking string that is aabcccccaa into a2b1cfa3
+
+function stringComp(string) {
+
+    let compressed = ""; // empty string 
+   
+    let messageArray = string.split(""); // split string into array to iterate 
+   
+     for (let i = 0; i < messageArray.length; i++){
+       let count = 1;
+       let currentLetter = messageArray[i];
+   
+       while (i < messageArray.length - 1 && messageArray[i] === messageArray[i + 1]){
+           count++;
+           i++;
+        }   
+         if (count === 1){
+           compressed += currentLetter;
+         } else {
+           compressed += currentLetter + count;
+         }
+   
+     }
+   
+    if ( string.length <= compressed.length ) {
+       return string;
+    } else {
+       return compressed;
+    }
+   
+   }
